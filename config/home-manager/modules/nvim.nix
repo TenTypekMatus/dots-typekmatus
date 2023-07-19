3bcb912dd529b188c646c42db669349f2f1c2378
Matus Mastena <Shadiness9530@proton.me>
2023-07-19 09:19:52 +0200

    Update

--- config/home-manager/modules/nvim.nix
+++ config/home-manager/modules/nvim.nix
@@ -5,10 +5,10 @@
     enable = true;
     plugins = with pkgs.vimPlugins; [
       vim-fugitive
+      nvim-colorizer-lua
       ultisnips
       vim-snippets
       nord-nvim
-      ale
       vim-devicons
       ansible-vim
       vim-autoformat
@@ -34,38 +34,42 @@
       cmp-nvim-lsp
       cmp-nvim-ultisnips
       lualine-nvim
+      nvim-notify
     ];
     extraConfig = ''
-      set encoding=utf-8
-      set nobackup
-      set nowritebackup
-      set nowildmenu
-      set updatetime=300
-      let g:barbar_auto_setup = v:true
-      set signcolumn=yes
-      lua <<EOF
-      require("noice").setup({
+    set encoding=utf-8
+    set nobackup
+    set nowritebackup
+    set nowildmenu
+    set updatetime=300
+    let g:barbar_auto_setup = v:true
+    set signcolumn=yes
+    packadd! nvim-colorizer.lua
+    lua require 'colorizer'.setup()
+    lua <<EOF
+    require("noice").setup({
       lsp = {
-      -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
-      override = {
-      ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-      ["vim.lsp.util.stylize_markdown"] = true,
-      ["cmp.entry.get_documentation"] = true,
-      },
+        -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+        override = {
+          ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+          ["vim.lsp.util.stylize_markdown"] = true,
+          ["cmp.entry.get_documentation"] = true,
+        },
       },
       -- you can enable a preset for easier configuration
       presets = {
-      bottom_search = true, -- use a classic bottom cmdline for search
-      command_palette = true, -- position the cmdline and popupmenu together
-      long_message_to_split = true, -- long messages will be sent to a split
-      inc_rename = false, -- enables an input dialog for inc-rename.nvim
-      lsp_doc_border = false, -- add a border to hover docs and signature help
+        bottom_search = true, -- use a classic bottom cmdline for search
+        command_palette = true, -- position the cmdline and popupmenu together
+        long_message_to_split = true, -- long messages will be sent to a split
+        inc_rename = false, -- enables an input dialog for inc-rename.nvim
+        lsp_doc_border = false, -- add a border to hover docs and signature help
       },
-      })
-      EOF
-      lua <<EOF
-      vim.o.showtabline = 2
-      local theme = {
+    })
+    EOF
+    au BufWritePost * lua require('lint').try_lint()
+    lua <<EOF
+    vim.o.showtabline = 2
+    local theme = {
       fill = 'TabLineFill',
       -- Also you can do this: fill = { fg='#f2e9de', bg='#907aa9', style='italic' }
       head = 'TabLine',
@@ -73,31 +77,31 @@
       tab = 'TabLine',
       win = 'TabLine',
       tail = 'TabLine',
-      }
-      require('tabby.tabline').set(function(line)
-      return {
+    }
+    require('tabby.tabline').set(function(line)
+    return {
       {
-      { '  ', hl = theme.head },
-      line.sep('', theme.head, theme.fill),
+        { 'i', hl = theme.head },
+        line.sep('', theme.head, theme.fill),
       },
       line.tabs().foreach(function(tab)
       local hl = tab.is_current() and theme.current_tab or theme.tab
       return {
         line.sep('', hl, theme.fill),
-        tab.is_current() and '' or '󰆣',
+        tab.is_current() and '' or '',
         tab.number(),
         tab.name(),
         tab.close_btn(''),
         line.sep('', hl, theme.fill),
         hl = hl,
-        margin = ' ',
+        margin = '  ',
       }
       end),
       line.spacer(),
       line.wins_in_tab(line.api.get_current_tab()).foreach(function(win)
       return {
         line.sep('', theme.win, theme.fill),
-        win.is_current() and '' or '',
+        win.is_current() and '' or '',
         win.buf_name(),
         line.sep('', theme.win, theme.fill),
         hl = theme.win,
@@ -105,122 +109,122 @@
       }
       end),
       {
-      line.sep('', theme.tail, theme.fill),
-      { '  ', hl = theme.tail },
+        line.sep('', theme.tail, theme.fill),
+        { '  ', hl = theme.tail },
       },
       hl = theme.fill,
-      }
-      end)
-      EOF
-      lua <<EOF
-      require("dashboard").setup({
+    }
+    end)
+    EOF
+    lua <<EOF
+    require("dashboard").setup({
       theme = 'hyper',
       config = {
-      week_header = {
-       enable = true,
-      },
-      shortcut = {
-        {
-          icon = ' ',
-          icon_hl = '@variable',
-          desc = 'Files',
-          group = 'Label',
-          action = 'Telescope find_files',
-          key = 'f',
+        week_header = {
+          enable = true,
         },
-        {
-          desc = ' Projects',
-          group = 'DiagnosticHint',
-          action = 'Telescope ~/proj',
-          key = 'a',
-        },
-        {
-          desc = ' dotfiles',
-          group = 'Number',
-          action = 'Telescope dotfiles',
-          key = 'd',
+        shortcut = {
+          {
+            icon = ' ',
+            icon_hl = '@variable',
+            desc = 'Files',
+            group = 'Label',
+            action = 'Telescope find_files',
+            key = 'f',
+          },
+          {
+            desc = ' Projects',
+            group = 'DiagnosticHint',
+            action = 'Telescope ~/proj',
+            key = 'a',
+          },
+          {
+            desc = ' dotfiles',
+            group = 'Number',
+            action = 'Telescope dotfiles',
+            key = 'd',
+          },
         },
       },
-      },
-      })
-      EOF
-      lua <<EOF
-      local cmp = require'cmp'
-      local lspkind = require'lspkind'
+    })
+    EOF
+    lua <<EOF
+    local cmp = require'cmp'
+    local lspkind = require'lspkind'
 
-      cmp.setup({
+    cmp.setup({
       snippet = {
-      expand = function(args)
-      -- For `ultisnips` user.
-      vim.fn["UltiSnips#Anon"](args.body)
-      end,
+        expand = function(args)
+        -- For `ultisnips` user.
+        vim.fn["UltiSnips#Anon"](args.body)
+        end,
       },
       mapping = cmp.mapping.preset.insert({
-          ['<Tab>'] = function(fallback)
-            if cmp.visible() then
-              cmp.select_next_item()
-            else
-              fallback()
-            end
-          end,
-          ['<S-Tab>'] = function(fallback)
-            if cmp.visible() then
-              cmp.select_prev_item()
-            else
-              fallback()
-            end
-          end,
-          ['<CR>'] = cmp.mapping.confirm({ select = true }),
-          ['<C-e>'] = cmp.mapping.abort(),
-          ['<Esc>'] = cmp.mapping.close(),
-          ['<C-d>'] = cmp.mapping.scroll_docs(-4),
-          ['<C-f>'] = cmp.mapping.scroll_docs(4),
-        }),
+        ['<Tab>'] = function(fallback)
+        if cmp.visible() then
+        cmp.select_next_item()
+        else
+        fallback()
+        end
+        end,
+        ['<S-Tab>'] = function(fallback)
+        if cmp.visible() then
+        cmp.select_prev_item()
+        else
+        fallback()
+        end
+        end,
+        ['<CR>'] = cmp.mapping.confirm({ select = true }),
+        ['<C-e>'] = cmp.mapping.abort(),
+        ['<Esc>'] = cmp.mapping.close(),
+        ['<C-d>'] = cmp.mapping.scroll_docs(-4),
+        ['<C-f>'] = cmp.mapping.scroll_docs(4),
+      }),
       sources = {
-      { name = 'nvim_lsp' }, -- For nvim-lsp
-      { name = 'ultisnips' }, -- For ultisnips user.
-      { name = 'nvim_lua' }, -- for nvim lua function
-      { name = 'path' }, -- for path completion
-      { name = 'buffer', keyword_length = 4 }, -- for buffer word completion
-      { name = 'omni' },
-      { name = 'emoji', insert = true, } -- emoji completion
+        { name = 'nvim_lsp' }, -- For nvim-lsp
+        { name = 'ultisnips' }, -- For ultisnips user.
+        { name = 'nvim_lua' }, -- for nvim lua function
+        { name = 'path' }, -- for path completion
+        { name = 'buffer', keyword_length = 4 }, -- for buffer word completion
+        { name = 'omni' },
+        { name = 'emoji', insert = true, } -- emoji completion
       },
       completion = {
-      keyword_length = 1,
-      completeopt = "menu,noselect"
+        keyword_length = 1,
+        completeopt = "menu,noselect"
       },
       view = {
-      entries = 'custom',
+        entries = 'custom',
       },
       formatting = {
-      format = lspkind.cmp_format({
-      mode = "symbol_text",
-      menu = ({
-        nvim_lsp = "[LSP]",
-        ultisnips = "[US]",
-        nvim_lua = "[Lua]",
-        path = "[Path]",
-        buffer = "[Buffer]",
-        emoji = "[Emoji]",
-          omni = "[Omni]",
-      }),
-      }),
+        format = lspkind.cmp_format({
+          mode = "symbol_text",
+          menu = ({
+            nvim_lsp = "[LSP]",
+            ultisnips = "[US]",
+            nvim_lua = "[Lua]",
+            path = "[Path]",
+            buffer = "[Buffer]",
+            emoji = "[Emoji]",
+            omni = "[Omni]",
+          }),
+        }),
       },
-      })
-      EOF
-      lua require'lspconfig'.vls.setup{}
-      lua require("lualine").setup()
-      set noshowmode
-      set mouse=a
-      set number
-      colorscheme nord
-      set shell=sh
-      set encoding=utf-8
-      nnoremap <leader>nt :tabnew<CR>
-      nnoremap <leader>tn :tabnext<CR>
-      nnoremap <leader>tc :tabclose<CR>
-      nnoremap <leader>ff :NERDTreeToggle<CR>
-      let g:NERDTreeGitStatusIndicatorMapCustom = {
+    })
+    EOF
+    lua require'lspconfig'.vls.setup{}
+    lua require("lualine").setup()
+    set noshowmode
+    set mouse=a
+    set number
+    colorscheme nord
+    set shell=sh
+    set encoding=utf-8
+    nnoremap <leader>nt :tabnew<CR>
+    nnoremap <leader>tn :tabnext<CR>
+    nnoremap <leader>tc :tabclose<CR>
+    nnoremap <leader>ff :NERDTreeToggle<CR>
+    let g:NERDTreeGitStatusIndicatorMapCustom = {
       \ 'Modified'  :'✹',
       \ 'Staged'    :'✚',
       \ 'Untracked' :'✭',
@@ -232,8 +236,8 @@
       \ 'Clean'     :'✔︎',
       \ 'Unknown'   :'?',
       \ }
-    '';
-    viAlias = true;
-    vimAlias = true;
-  };
-}
+      '';
+      viAlias = true;
+      vimAlias = true;
+    };
+  }
